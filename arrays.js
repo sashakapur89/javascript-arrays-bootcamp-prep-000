d2113dd7918edfa2287abb822f125c69f6294680
Sasha Kapur <sashakapur89@gmail.com>
2018-02-04 16:57:56 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -17,7 +17,7 @@ function destructivelyAddElementToBeginningOfArray(array, element){
 }
 
 function addElementToEndOfArray(array, element){
-  [...array, element] = array;
+  array = [...array, element];
   var newArray = array;
   return newArray;
 }
