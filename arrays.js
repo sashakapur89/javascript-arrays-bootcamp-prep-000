0e21a7967f68b98150db33b2a948be74f4ba21b9
Sasha Kapur <sashakapur89@gmail.com>
2018-02-04 16:44:48 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,23 @@
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles'
+  ];
+  
+function addElementToBeginningOfArray(array, element){
+ array = [element,...array];
+ var newArray= array
+ return newArray;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element);
+  return array;
+}
+
+function addElementToEndOfArray(array, element){
+  array.push(element);
+  var newArray = array;
+  return newArray;
+}
